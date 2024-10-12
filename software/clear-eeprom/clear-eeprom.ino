410e2813226239e1eb300de60f31bb7109d8a890
Konstantin Honcharov <k.honcharov@gmail.com>
2024-10-12 14:59:05 +0300

    fix: add mode

--- software/clear-eeprom/clear-eeprom.ino
+++ software/clear-eeprom/clear-eeprom.ino
@@ -1,12 +1,16 @@
 #include <EEPROM.h>
 
+#define MODE "read"
+
 void setup() {
   Serial.begin(9600);
   delay(5000);
 
   Serial.println(">>>");
   for (int i = 0; i < EEPROM.length(); i += 1) {
-    EEPROM.update(i, 0);
+    if (MODE == "update") {
+      EEPROM.update(i, 0);
+    }
     Serial.print(i);
     Serial.print(":");
     Serial.print(EEPROM.read(i));
