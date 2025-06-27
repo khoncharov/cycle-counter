cd4663b4ab22a0e2104a5d4d82bb45d0f829a58e
Konstantin Honcharov <k.honcharov@gmail.com>
2025-06-27 07:52:53 +0300

    feat: add set count func

--- software/clear-eeprom/clear-eeprom.ino
+++ software/clear-eeprom/clear-eeprom.ino
@@ -1,23 +1,40 @@
+/*
+MODE description:
+"read" - read eeprom and print to IDE
+"clear" - clear eeprom to all zeroes
+"set_count" - clear and set value
+*/
+
 #include <EEPROM.h>
 
 #define MODE "read"
+
+const unsigned int COUNT = 0  // MAX VALUE 65535
 
 void setup() {
   Serial.begin(9600);
   delay(5000);
 
   Serial.println(">>>");
-  for (int i = 0; i < EEPROM.length(); i += 1) {
-    if (MODE == "update") {
-      EEPROM.update(i, 0);
+  if (MODE == "set_count") {
+    EEPROM.put(0, COUNT);
+    Serial.println("<<<");
+  } else {
+    for (int i = 0; i < EEPROM.length(); i += 1) {
+      if (MODE == "read") {
+        Serial.print(i);
+        Serial.print(":");
+        Serial.print(EEPROM.read(i), HEX);
+        Serial.println();
+        delay(10);
+      }
+
+      if (MODE == "clear") {
+        EEPROM.update(i, 0);
+      }
     }
-    Serial.print(i);
-    Serial.print(":");
-    Serial.print(EEPROM.read(i), HEX);
-    Serial.println();
-    delay(10);
+    Serial.println("<<<");
   }
-  Serial.println("<<<");
 }
 
 void loop() {
